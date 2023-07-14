f60df04d961170b925dda1acc04362e1f0856dd6
Mateuszmeteo <mateusz.trepkowski@gmail.com>
2023-07-14 03:21:33 +0200

    notfound-test

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -7,8 +7,8 @@ export const NotFound = () => {
             <h2>Upsss...  Coś poszło nie tak...</h2>
             <h3>Przejdź na stronę:</h3>
             <p>
-                <a href="https://trepkowski-entertaiment.github.io/te"></a>
-                Trepkowski Entertainment
+                <a href="https://trepkowski-entertaiment.github.io/te">
+                    Trepkowski Entertainment</a>
             </p>
         </div>
     )
